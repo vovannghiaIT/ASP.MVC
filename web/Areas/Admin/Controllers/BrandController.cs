e77d79a83f5859047a2781b910866f399b6c8824
satookn <nghiak777@gmail.com>
2022-08-14 10:29:57 +0700

    add web file

---
+++ web/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web.Context;
+
+namespace VoVanNghia_2120110017.Areas.Admin.Controllers
+{
+    public class BrandController : Controller
+    {
+        QLBHEntities2 objQLBHEntities2 = new QLBHEntities2();
+        public ActionResult Index()
+        {
+            var listBrand = objQLBHEntities2.Brands.ToList();
+            return View(listBrand);
+        }
+    }
+}
---
+++ web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web.Context;
+
+namespace VoVanNghia_2120110017.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        QLBHEntities2 objQLBHEntities2 = new QLBHEntities2();
+        public ActionResult Index()
+        {
+            var listUser = objQLBHEntities2.Users.ToList();
+            return View(listUser);
+        }
+    }
+}
